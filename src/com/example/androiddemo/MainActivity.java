e026ce40abdf96989bd4043114aca634caa8708a
Yaguang Li <liyaguang0123@gmail.com>
2014-10-06 01:53:30 +0100

    Add gps location retrieval

--- src/com/example/androiddemo/MainActivity.java
+++ src/com/example/androiddemo/MainActivity.java
@@ -1,28 +1,121 @@
 package com.example.androiddemo;
 
+import android.content.Context;
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends ActionBarActivity {
 	public final static String EXTRA_MESSAGE = "com.example.androiddemo.MESSAGE";
+	private static final String TAG = "MainActivity";
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-//		setContentView(R.layout.activity_main);
+		// setContentView(R.layout.activity_main);
 		setContentView(R.layout.fragment_main);
+		// Init button
+//		final Button btnCommit = (Button) findViewById(R.id.commit_button);
+//		btnCommit.setOnClickListener(new View.OnClickListener() {
+//			
+//			@Override
+//			public void onClick(View v) {
+//				// TODO Auto-generated method stub
+//				getCurrentLocation();
+//			}
+//		});
+	}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		Log.d(TAG, "App paused");
+	}
+
+	@Override
+	protected void onStop() {
+		// TODO Auto-generated method stub
+		super.onStop();
+		Log.d(TAG, "App stoped");
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+		Log.d(TAG, "App resumed");
+	}
+
+	@Override
+	protected void onStart() {
+		// TODO Auto-generated method stub
+		super.onStart();
+		Log.d(TAG, "App Started");
+		boolean gpsEnabled = checkGPS();
+		Log.d(TAG, "GPS Enabled: " + gpsEnabled);
+	}
+	
+	public void btnCommitClick(View view) {
+		getCurrentLocation();
+		
+	}
+
+	private boolean checkGPS() {
+		// Check GPS
+		LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+		boolean gpsEnabled = locationManager
+				.isProviderEnabled(LocationManager.GPS_PROVIDER);
+		return gpsEnabled;
+	}
+	private void getCurrentLocation() {
+		LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+		locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, new LocationListener() {
+			
+			@Override
+			public void onStatusChanged(String provider, int status, Bundle extras) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void onProviderEnabled(String provider) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void onProviderDisabled(String provider) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void onLocationChanged(Location location) {
+				// TODO Auto-generated method stub
+				EditText editText = (EditText) findViewById(R.id.edit_message);
+				String locText = String.format("Lat: %.6f, lng: %.6f", location.getLatitude(), location.getLongitude());
+				Log.d(TAG, locText);
+				editText.setText(locText);
+			}
+		});
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		MenuInflater inflater = getMenuInflater();
-//		inflater.inflate(R.menu.main, menu);
+		// inflater.inflate(R.menu.main, menu);
 		inflater.inflate(R.menu.main_activity_actions, menu);
 		return super.onCreateOptionsMenu(menu);
 	}
@@ -38,8 +131,10 @@ public class MainActivity extends ActionBarActivity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+
 	/**
 	 * Called when click the button
+	 * 
 	 * @param view
 	 */
 	public void sendMessage(View view) {
@@ -49,11 +144,5 @@ public class MainActivity extends ActionBarActivity {
 		intent.putExtra(EXTRA_MESSAGE, message);
 		startActivity(intent);
 	}
+
 }
-
-
-
-
-
-
-
