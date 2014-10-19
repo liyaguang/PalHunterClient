269506f652ed54a6267ca0717ea6608465101036
Yaguang Li <liyaguang0123@gmail.com>
2014-10-19 00:06:06 -0700

    1. Change minimum requirement to Android 4.1

--- src/com/example/androiddemo/DisplayMessageActivity.java
+++ src/com/example/androiddemo/DisplayMessageActivity.java
@@ -1,13 +1,13 @@
 package com.example.androiddemo;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-public class DisplayMessageActivity extends ActionBarActivity {
+public class DisplayMessageActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
--- src/com/example/androiddemo/MainActivity.java
+++ src/com/example/androiddemo/MainActivity.java
@@ -1,21 +1,21 @@
 package com.example.androiddemo;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
+
 	public final static String EXTRA_MESSAGE = "com.example.androiddemo.MESSAGE";
 	private static final String TAG = "MainActivity";
 
